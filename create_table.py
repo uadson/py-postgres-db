4110da6754b6815514083e9730d272fdc2b179b8
uadson <uadsoncastro@gmail.com>
2021-05-24 08:25:58 -0300

    criado script para inserção de dados na tabela

--- create_table.py
+++ create_table.py
@@ -9,41 +9,41 @@ def create_tables():
 	commands = (
 
 		"""
-		CREATE TABLE vendedores (
-			vendedores_id SERIAL PRIMARY KEY,
-			vendedor_nome VARCHAR(255) NOT NULL
+		CREATE TABLE vendors (
+			vendor_id SERIAL PRIMARY KEY,
+			vendor_name VARCHAR(255) NOT NULL
 		)
 		""",
 
 		"""
-		CREATE TABLE papeis (
-			papel_id SERIAL PRIMARY KEY,
-			papel_nome VARCHAR(255) NOT NULL
+		CREATE TABLE parts (
+			part_id SERIAL PRIMARY KEY,
+			part_name VARCHAR(255) NOT NULL
 		)	
 		""",
 
 		"""
-		CREATE TABLE papel_desenho (
-			papel_id INTEGER PRIMARY KEY,
-			extensao_arquivo VARCHAR(5) NOT NULL,
-			dados_desenho BYTEA NOT NULL,
-			FOREIGN KEY (papel_id)
-			REFERENCES papeis (papel_id)
+		CREATE TABLE part_drawings (
+			part_id INTEGER PRIMARY KEY,
+			file_extension VARCHAR(5) NOT NULL,
+			drawing_data BYTEA NOT NULL,
+			FOREIGN KEY (part_id)
+			REFERENCES parts (part_id)
 			ON UPDATE CASCADE ON DELETE CASCADE
 		)
 		""",
 
 		"""
-		CREATE TABLE vendedor_papeis (
-			vendedores_id INTEGER NOT NULL, 
-			papel_id INTEGER NOT NULL,
-			PRIMARY KEY (vendedores_id, papel_id),
-			FOREIGN KEY (vendedores_id)
-			REFERENCES vendedores (vendedores_id)
+		CREATE TABLE vendor_parts (
+			vendor_id INTEGER NOT NULL, 
+			part_id INTEGER NOT NULL,
+			PRIMARY KEY (vendor_id, part_id),
+			FOREIGN KEY (vendor_id)
+			REFERENCES vendors (vendor_id)
 			ON UPDATE CASCADE ON DELETE CASCADE,
 
-			FOREIGN KEY (papel_id)
-			REFERENCES papeis (papel_id)
+			FOREIGN KEY (part_id)
+			REFERENCES parts (part_id)
 			ON UPDATE CASCADE ON DELETE CASCADE
 		)
 		""")
